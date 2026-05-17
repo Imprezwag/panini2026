5e43fd9da7aee008c7d8079ac4f81098ec0852c2
Imprezwag <santi.garcia92@gmail.com>
2026-05-17 04:29:14 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,19 @@
-const CACHE = 'panini2026-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
-
+const CACHE = 'panini2026-v2';
+const FILES = [
+  '/panini2026/',
+  '/panini2026/index.html',
+  '/panini2026/manifest.json',
+  '/panini2026/es/',
+  '/panini2026/es/index.html',
+  '/panini2026/en/',
+  '/panini2026/en/index.html'
+];
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(FILES))
   );
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
@@ -16,9 +22,6 @@ self.addEventListener('activate', e => {
   );
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
-  );
-});
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cach
